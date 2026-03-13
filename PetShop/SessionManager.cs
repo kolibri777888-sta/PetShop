2dede6f6235266e905225d7a2ee0642f7a7ba9ae
МОСЯГИН АНТОН 25.11.2005 <anton.mosyagin.is22b@gmail.com>
2026-03-13 12:26:31 +0300

    CLOSE #2. Создал класс SessionManager

---
+++ PetShop/SessionManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace PetShop
+{
+    public static class SessionManager
+    {
+        private static Timer inactivityTimer;
+        private static int timeoutSeconds;
+        private static Form currentForm;
+
+        public static void Initialize(Form form)
+        {
+            currentForm = form;
+
+            string timeoutStr = ConfigurationManager.AppSettings["SessionTimeoutSeconds"];
+            if (!int.TryParse(timeoutStr, out timeoutSeconds))
+                timeoutSeconds = 30;
+
+            inactivityTimer = new Timer();
+            inactivityTimer.Interval = timeoutSeconds * 1000;
+            inactivityTimer.Tick += InactivityTimer_Tick;
+            inactivityTimer.Start();
+
+            Application.AddMessageFilter(new ActivityMessageFilter());
+        }
+
+        public static void ResetTimer()
+        {
+            inactivityTimer?.Stop();
+            inactivityTimer?.Start();
+        }
+
+        private static void InactivityTimer_Tick(object sender, EventArgs e)
+        {
+            inactivityTimer.Stop();
+
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+
+            if (currentForm != null && !currentForm.IsDisposed)
+            {
+                currentForm.Hide();
+            }
+        }
+
+        private class ActivityMessageFilter : IMessageFilter
+        {
+            public bool PreFilterMessage(ref Message m)
+            {
+                if (m.Msg == 0x200 || m.Msg == 0x100)
+                {
+                    ResetTimer();
+                }
+                return false;
+            }
+        }
+    }
+}
